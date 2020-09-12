290e4f3792b8294886730043aaa9c95aaaf2e05e
Valentine <S21810006@universitas.klabat.ac.id>
2020-09-12 22:10:46 +0800

    first commit

--- pertemuan_7/index.js
+++ pertemuan_7/index.js
@@ -1,49 +1,89 @@
-//conditional //
+// //conditional //
 
-let nilaiValent = 95;
+// let nilaiValent = 95;
 
-if (nilaiValent > 80) {
-  console.log("SANGAT MEMUASKAN");
-} else if (nilaiValent >= 60 && nilaiValent <= 80) {
-  console.log("MEMUASKAN");
-} else {
-  console.log("TETAP SEMANGAT, COBA LAGI!");
-}
-
-
-//if, else if dan else
-let nilaiAndi = 95;
-
-if (nilaiAndi > 80) {
-  console.log("SANGAT MEMUASKAN");
-} else if (nilaiAndi >= 60 && nilaiAndi <= 80) {
-  console.log("MEMUASKAN");
-} else {
-  console.log("JANGAN MENYERAH, COBA LAGI!");
-}
-
-//penggunaan switch case //
+// if (nilaiValent > 80) {
+//   console.log("SANGAT MEMUASKAN");
+// } else if (nilaiValent >= 60 && nilaiValent <= 80) {
+//   console.log("MEMUASKAN");
+// } else {
+//   console.log("TETAP SEMANGAT, COBA LAGI!");
+// }
 
 
-let seragamSekolah;
-let hari = "senin";
+// //if,else if dan else
+// let nilaiValent = 95;
 
-switch (hari) {
-  case "senin":
-    seragamSekolah = "kemeja putih bawahan merah";
-    break;
-  case "selasa":
-    seragamSekolah = "kemeja hijau bawahan hitam";
-    break;
-  case "rabu":
-    seragamSekolah = "kemeja putih bawahan putih";
-    break;
-  case "kamis":
-    seragamSekolah = "kemeja batik merah bawahan hitam";
-    break;
-  case "jumat":
-    seragamSekolah = "baju pramuka";
-    break;
-  default:
-    seragamSekolah = "baju bebas";
-}
+// if (nilaiValent > 80) {
+//   console.log("SANGAT MEMUASKAN");
+// } else if (nilaiValent >= 60 && nilaiValent <= 80) {
+//   console.log("MEMUASKAN");
+// } else {
+//   console.log("TETAP SEMANGAT, COBA LAGI!");
+// }
+
+// //penggunaan switch case //
+
+
+// let seragamSekolah;
+// let hari = "senin";
+
+// switch (hari) {
+//   case "senin":
+//     seragamSekolah = "kemeja putih bawahan merah";
+//     break;
+//   case "selasa":
+//     seragamSekolah = "kemeja hijau bawahan hitam";
+//     break;
+//   case "rabu":
+//     seragamSekolah = "kemeja putih bawahan putih";
+//     break;
+//   case "kamis":
+//     seragamSekolah = "kemeja batik merah bawahan hitam";
+//     break;
+//   case "jumat":
+//     seragamSekolah = "baju pramuka";
+//     break;
+//   default:
+//     seragamSekolah = "baju bebas";
+// }
+
+
+// //For loop
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// //
+
+// //For in loop
+// const buku = {
+//     judul: "Harry Potter and The Philosopher's Stone",
+//     pengarang: "J. K. Rowling",
+//     tahun: 1997
+//  };
+//  for (x in buku) {
+//    console.log(x, ':', buku[x]);
+//  }
+// //
+// //for of loop
+// const buku = ["Infinity Thanos"}
+// //
+// for (x of buku) {
+//   console.log(x);
+// }
+
+
+// //penggunaan while
+// let i = 1;
+
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// Do while loop
+let i = 1;
+
+do {
+  console.log(i);
+  i++;
+} while (i <= 10);
