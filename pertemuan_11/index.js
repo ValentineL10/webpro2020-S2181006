4c1424a3e9df9fa923c1a4d3cffe93838b6a1d47
Valentine <S21810006@universitas.klabat.ac.id>
2020-10-04 09:14:38 +0800

    first commit

--- pertemuan_11/index.js
+++ pertemuan_11/index.js
@@ -1,37 +1,37 @@
 //Arrow function 
-function driversLicence6(passedTest) {
-        const firstName = 'Valent';
-        let yearOfBirth;
+
+// function driversLicence6(passedTest) {
+    //     const firstName = 'Valent';
+    //     let yearOfBirth;
     
-        if (passedTest) {
-            yearOfBirth = 1990;
-        }
-        console.log(firstName + ', born in ' + yearOfBirth +
-            ', is now offically allowed to drive a car');
+    //     if (passedTest) {
+    //         yearOfBirth = 2001;
+    //     }
+    //     console.log(firstName + ', born in ' + yearOfBirth +
+    //         ', is now offically allowed to drive a car');
+    // }
+    // driversLicence6(true);
+    
+    // let i = 23;
+    
+    // for (let i = 0; i < 5; i++) {
+    //     console.log(i);
+    // }
+    
+    // console.log(i);
+    
+
+//  Exercise
+//  convert to ES6 Syntax
+
+const yearUntilRetirement = (year,firstName) => {
+    let age =28;
+    const retirement=65-age;
+    if(retirement > 0){
+      console.log(`${firstName} retired in ${retirement} years.`);
     }
-    driversLicence6(true);
-    
-    let i = 23;
-    
-    for (let i = 0; i < 5; i++) {
-        console.log(i);
+    else{
+      console.log(`${firstName} has already retired`);
     }
-    
-    console.log(i);
-    
-
-
-
- //Exercise
-//convert to ES6 Syntax
-// const yearUntilRetirement = (year,firstName) => {
-//     let age=28;
-//     const retirement=65-age;
-//     if(retirement > 0){
-//       console.log(`${firstName} retired in ${retirement} years. `);
-//     }
-//     else{
-//       console.log(`${firstName} has already retired`);
-//     }
-//   }
-//   yearUntilRetirement(2035,'jhon');
+  }
+  yearUntilRetirement(2035,'andi');
