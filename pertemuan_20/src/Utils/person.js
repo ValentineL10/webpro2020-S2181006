2e57fb8ff2b8dc207f60efd018dcd3b6eb048a3d
Valentine <S21810006@universitas.klabat.ac.id>
2020-11-10 23:50:42 +0800

    first commit

---
+++ pertemuan_20/src/Utils/person.js
@@ -0,0 +1,26 @@
+export const person = [
+    {
+        name: "Valentine ",
+        institution: "Unklab",
+        address: "Langowan",
+        phone: "0851xxxxxx",
+    },
+    {
+        name: "Sucina Sondakh",
+        institution: "Unima",
+        address: "Tomohon",
+        phone: "082xxxxxxx",
+    },
+    {
+        name: "Danica ",
+        institution: "Unsrat",
+        address: "Manado",
+        phone: "089xxxxxxx",
+    },
+    {
+        name: "Axcel",
+        institution: "Unklab",
+        address: "Manado",
+        phone: "081xxxxxxx",
+    },
+    ];
