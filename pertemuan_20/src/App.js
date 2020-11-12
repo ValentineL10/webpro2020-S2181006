9ff328712f9c736c76276da191cfe03517df08df
Valentine <S21810006@universitas.klabat.ac.id>
2020-11-12 23:41:43 +0800

    first commit

--- pertemuan_20/src/App.js
+++ pertemuan_20/src/App.js
@@ -28,45 +28,45 @@
 import React from 'react';
 import './App.css';
 const sortByPower = (a, b) => {
-    return b.power - a.power;
+         return b.power - a.power;
 }
-const List = props => {
-return (
+        const List = props => {
+        return (
 <>
-<h1>React People!</h1>
+        <h1>React People!</h1>
 
-<ul className="list">
-{props.data.sort(sortByPower).map((person) => (
-<li key={person.name} className="list-item">
-<div className="list-item-image-container">
-<img src={person.thumbnail} />
-</div>
-<span className="list-item-name">
-{person.name} ( Power:{' '}
-<span className="list-item-power">{person.power}</span> )
-</span>
-</li>
-))}
-</ul>
-</>
+        <ul className="list">
+        {props.data.sort(sortByPower).map((person) => (
+        <li key={person.name} className="list-item">
+        <div className="list-item-image-container">
+        <img src={person.thumbnail} />
+        </div>
+        <span className="list-item-name">
+        {person.name} ( Power:{' '}
+        <span className="list-item-power">{person.power}</span> )
+        </span>
+        </li>
+        ))}
+        </ul>
+        </>
 );
 }
 const App = () => {
 const human = [
 {
-name: 'Kuririn',
+name: ' Mochi',
 thumbnail:
         'https://upload.wikimedia.org/wikipedia/pt/6/63/Kuririn_42311.png',
 power: 10
 },
 {
-name: 'Bulma',
+name: 'Lulafa',
 thumbnail: 
         'https://upload.wikimedia.org/wikipedia/hu/1/1c/Bulma.png',
 power: 3
 },
 {
-name: 'Chi-chi',
+name: 'Puppies',
 thumbnail:
         'https://upload.wikimedia.org/wikipedia/hu/8/84/Chi-chi_dragon_ball_anime.ppg',
 
